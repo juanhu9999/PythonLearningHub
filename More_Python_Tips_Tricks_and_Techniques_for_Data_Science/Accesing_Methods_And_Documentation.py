4f92a41da3fb3b6ee3a09cc190e7d53fd5abf9fa
juanhu9999 <juanpablo.isabella@gmail.com>
2024-08-23 12:27:35 -0300

    first tips

---
+++ More_Python_Tips_Tricks_and_Techniques_for_Data_Science/Accesing_Methods_And_Documentation.py
@@ -0,0 +1,9 @@
+## For example: we can use the help function to learn about the sum function in python.
+
+#help(sum)
+
+## In packages is similar, for example in the functions fo the pandas package
+
+from pandas import read_csv
+print(help(read_csv))
+
