258598933b441d29c6cdd6d3317e50ba24901068
juanhu9999 <juanpablo.isabella@gmail.com>
2024-08-26 17:34:53 -0300

    starting with arrays, average example

---
+++ More_Python_Tips_Tricks_and_Techniques_for_Data_Science/Essentials_Of_NumPy_arrays.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+# one dimension array
+
+a = np.array([1, 2, 3])
+
+print(a)
+
+# two dimension array
+
+b = np.array([[1, 2, 3], [4, 5, 6]])
+
+print(b)
+
+# Student average
+
+average = np.array([85, 90, 78, 92, 88])
+
+# Calculate the average
+
+average_calculate = np.mean(average)
+
+print("average:", average_calculate)
